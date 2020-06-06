ec0cb868d227033352fadde0b017ec3016bae723
Kelsey Dickson <kelseymedved@gmail.com>
2020-06-06 20:54:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -68,7 +68,7 @@ def adding_matz
 	   :known_for => "Ruby",
 	   :languages => ["LISP", "C"]
 	 },
- 		{
+ 		
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -82,7 +82,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
-} 
+}
 end
 
 def changing_alan
